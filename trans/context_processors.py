508664e1ad5aad18b641040784c0acd6d6cb7930
Andrius Žiūkas <andriusa2@gmail.com>
2024-04-15 03:22:02 +0100

    Update assets with BOI 2024 logo/titles.

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -2,8 +2,8 @@ from django.conf import settings
 
 def ioi_settings(request):
     return {'settings': {
-        'SITE_TITLE': 'IOI Task Translation System',
-        'CONTEST_TITLE': 'IOI',
+        'SITE_TITLE': 'BOI 2024 Task Translation System',
+        'CONTEST_TITLE': 'BOI 2024',
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': '/media/images/',
     }}
